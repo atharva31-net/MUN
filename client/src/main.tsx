fe1023f50c2e570efbd909a9149dd1222854186c
atharva31-net <atharvamake@gmail.com>
2025-06-27 23:15:14 +0530

    Fix Tailwind build, add logo, configure PostCSS

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "@fontsource/inter"; // ✅ This imports the Inter font globally
 
 createRoot(document.getElementById("root")!).render(<App />);
